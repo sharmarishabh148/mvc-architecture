5417342f79ee7a63d99f137e3d8bff7368863c1b
Rishabh Sharma <sharmarishabh148@gmail.com>
2021-06-19 02:56:48 +0530

    Go Artifacts and HTTP GIN framewroks

---
+++ mvc/services/user_service_test.go
@@ -0,0 +1,54 @@
+package services
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/sharmari/mvc/domain"
+	"github.com/sharmari/mvc/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	userDaoMock usersDaoMock
+
+	getUserFunction func(userId int64) (*domain.User, *utils.ApplicationError)
+)
+
+func init() {
+	domain.UserDao = &usersDaoMock{}
+}
+
+type usersDaoMock struct{}
+
+func (m *usersDaoMock) GetUser(userId int64) (*domain.User, *utils.ApplicationError) {
+	return getUserFunction(userId)
+}
+
+func TestGetUserNotFoundInDatabase(t *testing.T) {
+	getUserFunction = func(userId int64) (*domain.User, *utils.ApplicationError) {
+		return nil, &utils.ApplicationError{
+			StatusCode: http.StatusNotFound,
+			Message:    "user 0 does not exists",
+		}
+	}
+
+	user, err := UsersService.GetUser(0)
+	assert.Nil(t, user)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusNotFound, err.StatusCode)
+	assert.EqualValues(t, "user 0 does not exists", err.Message)
+}
+
+func TestGetUserNoError(t *testing.T) {
+	getUserFunction = func(userId int64) (*domain.User, *utils.ApplicationError) {
+		return &domain.User{
+			Id: 123,
+		}, nil
+	}
+
+	user, err := UsersService.GetUser(123)
+	assert.Nil(t, err)
+	assert.NotNil(t, user)
+	assert.EqualValues(t, 123, user.Id)
+}
