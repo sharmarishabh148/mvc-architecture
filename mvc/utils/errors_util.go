6ad02d089bc076dc4441114e67e3c0d2e12ce81b
Rishabh Sharma <sharmarishabh148@gmail.com>
2021-06-20 10:07:45 +0530

    json Encoding error fixed

--- mvc/utils/errors_util.go
+++ mvc/utils/errors_util.go
@@ -1,7 +1,7 @@
 package utils
 
 type ApplicationError struct {
-	Message    string `josn:"message"`
-	StatusCode int    `josn:"status"`
-	Code       string `josn:"code"`
+	Message    string `json:"message"`
+	StatusCode int    `json:"status"`
+	Code       string `json:"code"`
 }
